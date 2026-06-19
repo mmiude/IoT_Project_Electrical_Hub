b2ad3c5f0fbc3bfa50b751545d6c213fab62595f
Mmiud <mmiude@outlook.com>
2026-06-19 09:49:06 +0300

    clean up started. Added gateway.c/.h and ZigbeeCoordinator.cpp/.h files

--- src/zigbee/zigbee_gateway.c
+++ src/zigbee/zigbee_gateway.c
@@ -1,8 +1,5 @@
-/*
- * SPDX-FileCopyrightText: 2021-2026 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
+/* Hardware abstraction implemented with C. 
+Handles Zigbee network, stack and raw signals.*/
 
 #include "esp_check.h"
 #include "esp_err.h"
@@ -13,350 +10,8 @@
 #include "ezbee/zha.h"
 #include "zigbee_gateway.h"
 
-static const char *TAG = "ZIGBEE_COORDINATOR";
-
-typedef struct {    //Coordinator
-    uint16_t short_address;
-    uint8_t endpoint;
-    bool online;
-    bool is_on; 
-
-    uint16_t current_divisor;
-    uint16_t current_multiplier;
-    uint16_t voltage_divisor;
-    uint16_t voltage_multiplier;
-    uint16_t power_divisor;
-    uint16_t power_multiplier;
-    bool support_metering;
-    uint32_t metering_divisor;
-    uint32_t metering_multiplier;
-
-    float active_power;
-    float voltage; 
-    float current;
-    float summation_kwh; 
-
-} smartplugInfo;
-
-static smartplugInfo smartPlug;
-
-esp_err_t send_configure_reporting(uint16_t dst_addr, uint8_t dst_ep);
-
-static ezb_err_t read_electrical_measurement_multipliers(uint16_t dst_addr, uint8_t dst_ep) // coordinator
-{
-    uint16_t metering_attrs[] = {
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_AC_POWER_MULTIPLIER_ID,
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_AC_POWER_DIVISOR_ID,
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_AC_VOLTAGE_DIVISOR_ID,
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_AC_VOLTAGE_MULTIPLIER_ID,
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_AC_CURRENT_MULTIPLIER_ID,
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_AC_CURRENT_DIVISOR_ID  
-    };
-
-    ezb_zcl_read_attr_cmd_t read_attr_cmd = {
-        .cmd_ctrl = {
-            .dst_addr = {
-                .addr_mode = EZB_ADDR_MODE_SHORT,
-                .u = {
-                    .short_addr = dst_addr,
-                }
-            },
-            .dst_ep = dst_ep,
-            .src_ep = ESP_ZIGBEE_CUSTOM_GATEWAY_EP_ID,
-            .cluster_id = EZB_ZCL_CLUSTER_ID_ELECTRICAL_MEASUREMENT,
-            .manuf_code = EZB_ZCL_STD_MANUF_CODE,
-            .fc = {
-                .manuf_specific = 0,
-                .direction = 0,
-                .dis_default_rsp = 0,
-            },
-            .cnf_ctx = {
-                .cb = 0,
-            },
-        },
-        .payload = {
-            .attr_number = 6,
-            .attr_field = metering_attrs,
-        },
-    };
-
-    ezb_err_t err = ezb_zcl_read_attr_cmd_req(&read_attr_cmd);
-    return err == EZB_ERR_NONE ? ESP_OK : ESP_FAIL; 
-
-}
-
-static ezb_err_t read_electrical_measurement_values(uint16_t dst_addr, uint8_t dst_ep) // coordinator
-{
-    uint16_t metering_attrs[] = {
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_ACTIVE_POWER_ID,
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_RMS_VOLTAGE_ID,
-        EZB_ZCL_ATTR_ELECTRICAL_MEASUREMENT_RMS_CURRENT_ID,
-    };
-
-    ezb_zcl_read_attr_cmd_t read_attr_cmd = {
-        .cmd_ctrl = {
-            .dst_addr = {
-                .addr_mode = EZB_ADDR_MODE_SHORT,
-                .u = {
-                    .short_addr = dst_addr,
-                }
-            },
-            .dst_ep = dst_ep,
-            .src_ep = ESP_ZIGBEE_CUSTOM_GATEWAY_EP_ID,
-            .cluster_id = EZB_ZCL_CLUSTER_ID_ELECTRICAL_MEASUREMENT,
-            .manuf_code = EZB_ZCL_STD_MANUF_CODE,
-            .fc = {
-                .manuf_specific = 0,
-                .direction = 0,
-                .dis_default_rsp = 0,
-            },
-            .cnf_ctx = {
-                .cb = 0,
-            },
-        },
-        .payload = {
-            .attr_number = 3,
-            .attr_field = metering_attrs,
-        },
-    };
-
-    ezb_err_t err = ezb_zcl_read_attr_cmd_req(&read_attr_cmd);
-    return err == EZB_ERR_NONE ? ESP_OK : ESP_FAIL; 
-}
-
-static ezb_err_t read_energy_consumption_multipliers(uint16_t dst_addr, uint8_t dst_ep) // coordinator
-{
-    uint16_t metering_attrs[] = {
-        EZB_ZCL_ATTR_METERING_MULTIPLIER_ID,                   // 0x0301 - to convert raw value
-        EZB_ZCL_ATTR_METERING_DIVISOR_ID, 
-    };
-
-    ezb_zcl_read_attr_cmd_t read_attr_cmd = {
-        .cmd_ctrl = {
-            .dst_addr = {
-                .addr_mode = EZB_ADDR_MODE_SHORT,
-                .u = {
-                    .short_addr = dst_addr,
-                }
-            },
-            .dst_ep = dst_ep,
-            .src_ep = ESP_ZIGBEE_CUSTOM_GATEWAY_EP_ID,
-            .cluster_id = EZB_ZCL_CLUSTER_ID_METERING,
-            .manuf_code = EZB_ZCL_STD_MANUF_CODE,
-            .fc = {
-                .manuf_specific = 0,
-                .direction = 0,
-                .dis_default_rsp = 0,
-            },
-            .cnf_ctx = {
-                .cb = 0,
-            },
-        },
-        .payload = {
-            .attr_number = 2,
-            .attr_field = metering_attrs,
-        },
-    };
-
-    ezb_err_t err = ezb_zcl_read_attr_cmd_req(&read_attr_cmd);
-    return err == EZB_ERR_NONE ? ESP_OK : ESP_FAIL; 
-}
-
-static ezb_err_t read_energy_consumption_value(uint16_t dst_addr, uint8_t dst_ep) // coordinator
-{
-    uint16_t metering_attrs[] = {
-        EZB_ZCL_ATTR_METERING_CURRENT_SUMMATION_DELIVERED_ID,  
-    };
-
-    ezb_zcl_read_attr_cmd_t read_attr_cmd = {
-        .cmd_ctrl = {
-            .dst_addr = {
-                .addr_mode = EZB_ADDR_MODE_SHORT,
-                .u = {
-                    .short_addr = dst_addr,
-                }
-            },
-            .dst_ep = dst_ep,
-            .src_ep = ESP_ZIGBEE_CUSTOM_GATEWAY_EP_ID,
-            .cluster_id = EZB_ZCL_CLUSTER_ID_METERING,
-            .manuf_code = EZB_ZCL_STD_MANUF_CODE,
-            .fc = {
-                .manuf_specific = 0,
-                .direction = 0,
-                .dis_default_rsp = 0,
-            },
-            .cnf_ctx = {
-                .cb = 0,
-            },
-        },
-        .payload = {
-            .attr_number = 1,
-            .attr_field = metering_attrs,
-        },
-    };
-
-    ezb_err_t err = ezb_zcl_read_attr_cmd_req(&read_attr_cmd);
-    return err == EZB_ERR_NONE ? ESP_OK : ESP_FAIL; 
-}
-
-static esp_err_t read_plug_on_off_state(uint16_t dst_addr, uint8_t dst_ep) // coordinator
-{
-    uint16_t on_off_attrs[] = {EZB_ZCL_ATTR_ON_OFF_ON_OFF_ID};
-
-    ezb_zcl_read_attr_cmd_t read_attr_cmd = {
-        .cmd_ctrl = {
-            .dst_addr = {
-                .addr_mode = EZB_ADDR_MODE_SHORT, 
-                .u = {
-                    .short_addr = dst_addr,
-                }
-            },
-            .dst_ep = dst_ep,
-            .src_ep = ESP_ZIGBEE_CUSTOM_GATEWAY_EP_ID,
-            .cluster_id = EZB_ZCL_CLUSTER_ID_ON_OFF,
-            .manuf_code = EZB_ZCL_STD_MANUF_CODE,
-            .fc = {
-                .manuf_specific = 0,
-                .direction = 0,
-                .dis_default_rsp = 0,
-            },
-            .cnf_ctx = {
-                .cb = 0,
-            },
-        },
-        .payload = {
-            .attr_number = 1,
-            .attr_field = on_off_attrs,
-        }
-    };
-
-    ezb_err_t err = ezb_zcl_read_attr_cmd_req(&read_attr_cmd);
-    return err == EZB_ERR_NONE ? ESP_OK : ESP_FAIL; 
-}
-
-static esp_err_t send_toggle_smart_plug(uint16_t dst_addr, uint8_t dst_ep) // coordinator
-{
-    ezb_zcl_on_off_cmd_t toggle_cmd = {
-        .cmd_ctrl = {
-            .dst_addr = {
-                .addr_mode = EZB_ADDR_MODE_SHORT,
-                .u = {
-                    .short_addr = dst_addr,
-                }
-            },
-            .dst_ep = dst_ep,
-            .src_ep = ESP_ZIGBEE_CUSTOM_GATEWAY_EP_ID,
-            .dis_default_rsp = false,
-            .cnf_ctx = {
-                .cb = 0,
-            },
-        }
-    };
-
-    ezb_err_t err = ezb_zcl_on_off_toggle_cmd_req(&toggle_cmd);
-    ESP_LOGI(TAG, "Toggle command result: 0x%04x", err);
-    
-    return err == EZB_ERR_NONE ? ESP_OK : ESP_FAIL; 
-}
-
-static void zdo_bind_smart_plug_result(const ezb_zdp_bind_req_result_t *result, void *user_ctx) // coordinator 
-{
-    assert(result);
-    if (result->error == EZB_ERR_NONE) {
-        if (result->rsp && result->rsp->status == EZB_ZDP_STATUS_SUCCESS) {
-            ESP_LOGI(TAG, "Bound smart plug device successfully");
-            //ESP_LOGW(TAG, "SHORT ADDRESS: %d", (uint16_t) user_ctx);
-            //send_configure_reporting(smart_plugs[smart_index], 1);
-            esp_zigbee_lock_acquire(portMAX_DELAY);
-            send_configure_reporting(smartPlug.short_address, smartPlug.endpoint);
-            read_electrical_measurement_multipliers(smartPlug.short_address, smartPlug.endpoint);
-            read_energy_consumption_multipliers(smartPlug.short_address, smartPlug.endpoint);
-            esp_zigbee_lock_release(); 
-
-        } else {
-            ESP_LOGE(TAG, "Failed to bind smart plug device with status (0x%02x)", result->rsp->status);
-        }
-    } else {
-        ESP_LOGE(TAG, "Failed to bind smart plug device with error (0x%04x)", result->error);
-    }
-}
-
-static ezb_err_t zdo_bind_smart_plug_device(uint16_t dst_short_addr, uint8_t dst_ep) // coordinator
-{
-    ezb_err_t          ret      = EZB_ERR_FAIL;
-    ESP_LOGW(TAG, "plug short address: %d", dst_short_addr);
-
-    ezb_zdo_bind_req_t bind_req = {
-        .dst_nwk_addr = dst_short_addr,
-        .field =
-            {
-                .src_ep        = dst_ep, 
-                .dst_addr_mode = EZB_ADDR_MODE_EXT,
-                .dst_ep        = ESP_ZIGBEE_CUSTOM_GATEWAY_EP_ID,
-                .cluster_id = EZB_ZCL_CLUSTER_ID_ON_OFF,
-            },
-        .cb       = zdo_bind_smart_plug_result,
-        //.user_ctx = (void *) dst_short_addr, //(void*) dst_short_addr,
-    };
-    ezb_address_extended_by_short(dst_short_addr, &bind_req.field.src_addr);
-    ezb_nwk_get_extended_address(&bind_req.field.dst_addr.extended_addr);
-
-    ret = ezb_zdo_bind_req(&bind_req);
-
-    if (ret == EZB_ERR_NONE) {
-        ESP_LOGI(TAG, "Attempt to bind smart plug device (short address: 0x%04hx)", dst_short_addr);
-    } else {
-        ESP_LOGE(TAG, "Failed to bind smart plug device (short address: 0x%04hx) with error(0x%04x)", dst_short_addr, ret);
-    }
-    return ret;
-}
-
-static void zdo_find_smart_plug_device_result(const ezb_zdo_match_desc_req_result_t *result, void *user_ctx) // coordinator
-{
-    assert(result);
-    if (result->error == EZB_ERR_NONE) {
-        if (result->rsp && result->rsp->status == EZB_ZDP_STATUS_SUCCESS && result->rsp->match_length > 0 &&
-            result->rsp->match_list) {
-            for (size_t i = 0; i < result->rsp->match_length; i++) {
-                ESP_LOGW(TAG, "possible id: %d", result->rsp->match_list[i]);
-                smartPlug.endpoint = result->rsp->match_list[i];
-                zdo_bind_smart_plug_device(result->rsp->nwk_addr_of_interest, result->rsp->match_list[i]);
-            }
-        }
-    } else {
-        ESP_LOGE(TAG, "Failed to find smart plug device in the network with error(0x%04x)", result->error);
-    }
-}
-
-static ezb_err_t zdo_find_smart_plug_device(uint16_t dst_addr) // coordinator
-{   
-    smartPlug.short_address = dst_addr;
-
-    ezb_err_t ret             = EZB_ERR_FAIL;
-    uint16_t  cluster_list[] = {EZB_ZCL_CLUSTER_ID_ON_OFF};
-
-    ezb_zdo_match_desc_req_t req = {
-        .dst_nwk_addr = dst_addr,
-        .field =
-            {
-                .nwk_addr_of_interest = dst_addr,
-                .profile_id           = EZB_AF_HA_PROFILE_ID,
-                .num_in_clusters      = sizeof(cluster_list) / sizeof(cluster_list[0]),
-                .num_out_clusters     = 0,
-                .cluster_list         = cluster_list,
-            },
-        .cb       = zdo_find_smart_plug_device_result,
-        .user_ctx = NULL,
-    };
-    ret = ezb_zdo_match_desc_req(&req);
-    if (ret == EZB_ERR_NONE) {
-        ESP_LOGI(TAG, "Attempt to find smart_plug device");
-    } else {
-        ESP_LOGE(TAG, "Failed to find smart_plug device with error(0x%04x)", ret);
-    }
-    return ret;
-} 
-
+static const char *TAG = "ZIGBEE_GATWAY";
+ 
 static void esp_zigbee_alarm_bdb_commissioning(alarm_timer_arg_t arg) // gateway 
 {
     //mandatory to acquire the lock before calling any Zigbee SDK APIs
@@ -365,7 +20,6 @@ static void esp_zigbee_alarm_bdb_commissioning(alarm_timer_arg_t arg) // gateway
     esp_zigbee_lock_release();
 }
 
-//think of this like a interrupt handler for zigbee signals
 static bool esp_zigbee_app_signal_handler(const ezb_app_signal_t *app_signal) // gateway
 {   
     //Obtains the type of the application signal 
@@ -415,18 +69,18 @@ static bool esp_zigbee_app_signal_handler(const ezb_app_signal_t *app_signal) //
             alarm_timer_schedule(esp_zigbee_alarm_bdb_commissioning, EZB_BDB_MODE_NETWORK_FORMATION, 1000);
         }
     } break;
-    case EZB_ZDO_SIGNAL_DEVICE_ANNCE: {
+    case EZB_ZDO_SIGNAL_DEVICE_ANNCE: { // TODO: how to forward short_addr to coordinator class? 
         const ezb_zdo_signal_device_annce_params_t *dev_annce_params = ezb_app_signal_get_params(app_signal);
         ESP_LOGI(TAG, "New device commissioned or rejoined (short: 0x%04hx)", dev_annce_params->short_addr); // devices shrot address 
         zdo_find_smart_plug_device(dev_annce_params->short_addr);
         //smart_plugs = dev_annce_params->short_addr;
         //send_configure_reporting(dev_annce_params->short_addr, 1);
     } break;
-    case EZB_ZDO_SIGNAL_LEAVE_INDICATION: {
+    case EZB_ZDO_SIGNAL_LEAVE_INDICATION: { // TODO: how to forward short_addr to coordinator class? 
         const ezb_zdo_signal_leave_indication_params_t *leave_ind_params = ezb_app_signal_get_params(app_signal);
         ESP_LOGI(TAG, "Zigbee Node(0x%04hx) is leaving network", leave_ind_params->short_addr); // leaving devices information in leave_ind_params
     } break;
-    case EZB_NWK_SIGNAL_PERMIT_JOIN_STATUS: {
+    case EZB_NWK_SIGNAL_PERMIT_JOIN_STATUS: { // TODO: how can we open network once requested? 
         uint8_t duration = *(uint8_t *)ezb_app_signal_get_params(app_signal);
         if (duration) {
             ESP_LOGI(TAG, "Network(0x%04hx) is open for %d seconds", ezb_nwk_get_panid(), duration); // network ezb_nwk_get_panid() gives network id 
@@ -465,6 +119,7 @@ static void zcl_core_report_attr_handler(ezb_zcl_cmd_report_attr_message_t *mess
         ESP_LOGW(TAG, "Unhandled report cluster (0x04x)", message->info.cluster_id);
     }
 }
+
 
 static void zcl_core_read_attrbute_response(ezb_zcl_cmd_read_attr_rsp_message_t *message)
 {
@@ -566,6 +221,7 @@ static void zcl_core_read_attrbute_response(ezb_zcl_cmd_read_attr_rsp_message_t 
     }
 }
 
+
 static void esp_zigbee_zcl_core_action_handler(ezb_zcl_core_action_callback_id_t callback_id, void *message) {
 
     ESP_LOGI(TAG, "ZCL action callback ID: 0x%04lx", callback_id);
@@ -616,53 +272,6 @@ static void esp_zigbee_zcl_core_action_handler(ezb_zcl_core_action_callback_id_t
     }
 }
 
-esp_err_t send_configure_reporting(uint16_t dst_addr, uint8_t dst_ep)
-{
-    // ON/OFF cluster
-    // uint8_t onoff_change = 0;
-    ezb_zcl_config_report_record_t onoff_records[] = {
-        {
-            .direction = EZB_ZCL_REPORTING_SEND,
-            .attr_id = EZB_ZCL_ATTR_ON_OFF_ON_OFF_ID,
-            .client = {
-                .attr_type = EZB_ZCL_ATTR_TYPE_BOOL,
-                .min_interval = 0,
-                .max_interval = 43200,
-                .reportable_change = { .u8 = 1},
-            },
-        },
-    };
-    ezb_zcl_config_report_cmd_t onoff_cmd = {    
-        .cmd_ctrl = {
-            .dst_addr = {
-                .addr_mode = EZB_ADDR_MODE_SHORT,
-                .u.short_addr =  dst_addr
-            },
-            .dst_ep = dst_ep,
-            .src_ep = ESP_ZIGBEE_CUSTOM_GATEWAY_EP_ID,
-            .cluster_id = EZB_ZCL_CLUSTER_ID_ON_OFF,
-            .manuf_code = EZB_ZCL_STD_MANUF_CODE,
-            .fc = {
-                .manuf_specific = 0,
-                .direction = 0,
-                .dis_default_rsp = 0,
-            },
-        },
-        .payload = {
-            .record_number = 1,
-            .record_field = onoff_records,
-        }
-    };
-    ezb_err_t err = ezb_zcl_config_report_cmd_req(&onoff_cmd);
-    ESP_LOGI(TAG, "Configure reporting ON/OFF result: 0x%04x", err);
-
-    vTaskDelay(pdMS_TO_TICKS(100)); 
-
-    return ESP_OK; 
-} 
-
-// here we need to check that should server be changed to client and see that all necessary clusters are added 
-// also probably some signals need to be added to signal handler
 esp_err_t esp_zigbee_create_zha_gateway_device(void)
 {
     ezb_af_device_desc_t dev_desc = ezb_af_create_device_desc();
@@ -698,55 +307,4 @@ esp_err_t esp_zigbee_setup_commissioning(void)
     ESP_ERROR_CHECK(ezb_app_signal_add_handler(esp_zigbee_app_signal_handler));
 
     return ESP_OK;
-}
-
-static void esp_zigbee_stack_main_task(void *pvParameters) // coordinator task
-{
-    esp_zigbee_config_t zigbee_config = ESP_ZIGBEE_DEFAULT_CONFIG();
-
-    ESP_ERROR_CHECK(esp_zigbee_init(&zigbee_config));
-
-    ESP_ERROR_CHECK(esp_zigbee_setup_commissioning());
-
-    ESP_ERROR_CHECK(esp_zigbee_create_zha_gateway_device());
-
-    ESP_ERROR_CHECK(esp_zigbee_start(false));
-
-    esp_zigbee_launch_mainloop();
-
-    esp_zigbee_deinit();
-
-    vTaskDelete(NULL);
-}
-
-static void dummy_toggle_task(void *pvParameters)
-{   
-    while (1) {
-
-        esp_zigbee_lock_acquire(portMAX_DELAY);
-        read_energy_consumption_value(smartPlug.short_address, smartPlug.endpoint);
-        read_electrical_measurement_values(smartPlug.short_address, smartPlug.endpoint);
-        esp_zigbee_lock_release(); 
-
-        vTaskDelay(pdMS_TO_TICKS(500));
-
-        printf("SMART PLUG: %d ****** INFO ***** \n", smartPlug.short_address);
-        printf("plug on/off state: %s\n", smartPlug.is_on ? "ON" : "OFF");
-        printf("current: %.4f A\n", smartPlug.current);
-        printf("voltage: %.2f V\n", smartPlug.voltage);
-        printf("active power: %.2f W\n", smartPlug.active_power);
-        printf("energy summation: %.2f kwh\n", smartPlug.summation_kwh);
-        vTaskDelay(pdMS_TO_TICKS(20000));
-        
-    }
-}
-
-void app_main(void)
-{
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(nvs_flash_init_partition(ESP_ZIGBEE_STORAGE_PARTITION_NAME));
-
-    ESP_LOGI(TAG, "Start ESP Zigbee Stack");
-    xTaskCreate(esp_zigbee_stack_main_task, "Zigbee_main", 4096 * 2, NULL, 5, NULL);
-    xTaskCreate(dummy_toggle_task, "Toggle test", 2048, NULL, tskIDLE_PRIORITY + 1, NULL);
 }
