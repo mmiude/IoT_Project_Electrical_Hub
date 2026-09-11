db89bc57933c6bdcb4daeed9c41c7b88c39fa529
Mmiud <mmiude@outlook.com>
2026-09-11 15:35:56 +0300

    base nvs class created. builds but not tested yet.

---
+++ main/storage/NvsStorage.cpp
@@ -0,0 +1,84 @@
+#include "NvsStorage.h"
+
+NvsStorage::NvsStorage(const std::string &nameSpace_n) : nameSpaceName(nameSpace_n) {
+    esp_err_t err = nvs_open(nameSpaceName.c_str(), NVS_READWRITE, &handle);
+    if (err != ESP_OK) {
+        ESP_LOGE("NVS", "failed to open: %s", nameSpaceName); 
+        handle = 0; 
+    }
+}
+
+NvsStorage::~NvsStorage() {
+    if (handle) {
+        nvs_commit(handle);
+        nvs_close(handle);
+    }
+}
+
+esp_err_t NvsStorage::read_string(const std::string &key, std::string &word) {
+    if (!handle) return ESP_ERR_NVS_INVALID_HANDLE;
+    size_t required_size = 0; 
+    esp_err_t err = nvs_get_str(handle, key.c_str(), NULL, &required_size);
+
+    if (err != ESP_OK ) {
+        ESP_LOGE("NVS", "Error (%s) reading string size!", esp_err_to_name(err));
+        return err;
+    } 
+
+    word.resize(required_size - 1);
+    return nvs_get_str(handle, key.c_str(), &word[0], &required_size);
+
+    /*if (err != ESP_OK) {
+        ESP_LOGE("NVS", "error reading string: %s", esp_err_to_name(err));
+        return err;
+    }
+
+    word = std::string(buffer.data());
+    return ESP_OK; */
+}
+
+esp_err_t NvsStorage::write_string(const std::string &key, const std::string &word) {
+    if (!handle) return ESP_ERR_NVS_INVALID_HANDLE; 
+    esp_err_t err = nvs_set_str(handle, key.c_str(), word.c_str());
+    if (err == ESP_OK) nvs_commit(handle);
+    return err;
+}
+
+esp_err_t NvsStorage::read_u8(const std::string &key, uint8_t &value) {
+    if (!handle) return ESP_ERR_NVS_INVALID_HANDLE; 
+    return nvs_get_u8(handle, key.c_str(), &value);
+}
+
+esp_err_t NvsStorage::write_u8(const std::string &key, const uint8_t &value) {
+    if (!handle) return ESP_ERR_NVS_INVALID_HANDLE;
+    esp_err_t err = nvs_set_u8(handle, key.c_str(), value);
+    if (err == ESP_OK) nvs_commit(handle);
+    return err;
+}
+
+esp_err_t NvsStorage::read_u32(const std::string &key, uint32_t &value) {
+    if (!handle) return ESP_ERR_NVS_INVALID_HANDLE;
+    return nvs_get_u32(handle, key.c_str(), &value);
+}
+
+esp_err_t NvsStorage::write_u32(const std::string &key, const uint32_t &value) {
+    if (!handle) return ESP_ERR_NVS_INVALID_HANDLE;
+    esp_err_t err = nvs_set_u32(handle, key.c_str(), value);
+    if (err == ESP_OK) nvs_commit(handle);
+    return err; 
+}
+
+esp_err_t NvsStorage::erase_key(const std::string &key) {
+    if (!handle) return ESP_ERR_NVS_INVALID_HANDLE;
+    esp_err_t err = nvs_erase_key(handle, key.c_str());
+    if (err == ESP_OK) nvs_commit(handle);
+    return err;
+}
+
+esp_err_t NvsStorage::erase_all() {
+    if (!handle) return ESP_ERR_NVS_INVALID_HANDLE;
+    esp_err_t err = nvs_erase_all(handle); 
+    if (err == ESP_OK) nvs_commit(handle);
+    return err; 
+}
+
---
+++ main/storage/NvsStorage.h
@@ -0,0 +1,50 @@
+#ifndef NVSSTORAGE_H
+#define NVSSTORAGE_H
+
+#include "nvs_flash.h"
+#include "esp_log.h"
+#include <string>
+
+class NvsStorage {
+public:
+    NvsStorage(const std::string &nameSpace_n);
+    ~NvsStorage();
+
+    // restricted to trivially copyable types only!! 
+    template<typename T>
+    esp_err_t read_blob(const std::string &key, T &data) { 
+        if (!handle) return ESP_ERR_NVS_INVALID_HANDLE; 
+        size_t size = sizeof(T);
+        return nvs_get_blob(handle, key.c_str(), &data, &size);
+    }
+
+    template<typename T>
+    esp_err_t write_blob(const std::string &key, const T &data){
+        if (!handle) return ESP_ERR_NVS_INVALID_HANDLE;
+        esp_err_t err = nvs_set_blob(handle, key.c_str(), &data, sizeof(T));
+        if (err == ESP_OK) nvs_commit(handle);
+        return err;
+    }
+
+    esp_err_t read_string(const std::string &key, std::string &word);
+    esp_err_t write_string(const std::string &key, const std::string &word);
+
+    esp_err_t read_u8(const std::string &key, uint8_t &value);
+    esp_err_t write_u8(const std::string &key, const uint8_t &value);
+
+    esp_err_t read_u32(const std::string &key, uint32_t &value);
+    esp_err_t write_u32(const std::string &key, const uint32_t &value);
+
+    esp_err_t read_float(const std::string &key, float &value);
+    esp_err_t write_float(const std::string &key, const float &value);
+
+    esp_err_t erase_key(const std::string &key);
+    esp_err_t erase_all();
+
+private:
+    nvs_handle_t handle = 0; 
+    std::string nameSpaceName; 
+
+};
+
+#endif //NVSSTORAGE_H
