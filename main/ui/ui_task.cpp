def687c6b8b2879794d78231038653df35eb8064
reneki <rene.kilpelainen@gmail.com>
2026-09-14 12:41:26 +0300

    fixed an idiotic typo

--- main/ui/ui_task.cpp
+++ main/ui/ui_task.cpp
@@ -4,7 +4,7 @@
 
 //NOTE; mirrors the dummy task in main.cpp! no display stuff yet
 
-static conts char *TAG = "UI_TASK";
+static const char *TAG = "UI_TASK";
 
 void ui_task(void *params){
 
@@ -14,14 +14,15 @@ void ui_task(void *params){
     ESP_LOGI(TAG, "UI task started, Zigbee stack ready");
 
     controller_data msg;
+
     while (true) {
         if (xQueueReceive(p->ui_queue, &msg, portMAX_DELAY) == pdPASS) {
             switch (msg.type) {
                 case DATA_TYPE_DEVICE_JOIN:
-                    ESP_LOGI(TAG, "device join: 0x%016llx", msg.device.id);
+                    ESP_LOGI(TAG, "device join: 0x%016llx", msg.device_id);
                     break;
                 case DATA_TYPE_DEVICE_LEFT:
-                    ESP_LOGI(TAG, "device left: 0x%016llx", msg.device.id);
+                    ESP_LOGI(TAG, "device left: 0x%016llx", msg.device_id);
                     break;
                 case DATA_TYPE_POWER:
                     ESP_LOGI(TAG, "device 0x%016llx power %.2f", msg.device_id, msg.data.value);
