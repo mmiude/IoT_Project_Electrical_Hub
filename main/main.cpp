d61163a3cf43c29578445d6abb55317b778c2524
Aleksi1212 <aleksinoro@gmail.com>
2026-09-14 19:50:34 +0300

    remove tb_command_q

--- main/main.cpp
+++ main/main.cpp
@@ -134,9 +134,9 @@ extern "C" void app_main(void)
     static QueueHandle_t controllerQueue = xQueueCreate(10, sizeof(controller_data)); // Hub controller receives all data from this queue. If task sends ANY data to controller it must be put here.
     static QueueHandle_t uiQueue = xQueueCreate(10, sizeof(controller_data)); // Hub controller sends data to local ui via this queue.
     static QueueHandle_t cloudQueue = xQueueCreate(10, sizeof(controller_data)); // Hub controller sends data to cloud via this queue - not yet implemented on controller side 
-    static QueueHandle_t tb_command_q = xQueueCreate(10, sizeof(HubCommand));
+    // static QueueHandle_t tb_command_q = xQueueCreate(10, sizeof(HubCommand));
 
-    CloudCommunication cloud_communication(&ipstack, wifi_eg, tb_command_q);
+    CloudCommunication cloud_communication(&ipstack, wifi_eg, /*tb_command_q, */controllerQueue);
 
     static std::vector<std::shared_ptr<IDeviceProtocol>> protocols = {
         std::make_shared<ZigbeeCoordinator>(controllerQueue, wifi_eg)
