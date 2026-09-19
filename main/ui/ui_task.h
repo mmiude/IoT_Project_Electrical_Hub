f8e964ad9c4ddbd1e5d23dafb819eec3b531a447
reneki <rene.kilpelainen@gmail.com>
2026-09-19 15:26:39 +0300

    changes in ui_task (ui_model not rdy yet)

--- main/ui/ui_task.h
+++ main/ui/ui_task.h
@@ -1,20 +1,33 @@
 #ifndef UI_TASK_H
 #define UI_TASK_H
 
+#include <memory>
+
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "freertos/queue.h"
 #include "freertos/event_groups.h"
 
-//NOTE; mirrors the dummy task in main.cpp! 
+#include "DeviceInfoStorage.h"
+#include "ui_model.h"
 
-struct UiTaskParams {
+// owns the local ui: creates its own task, starts lvgl + screens once
 
-    QueueHandle_t controller_queue;
+// zigbee is ready and feeds controller messages from ui_queue into the uimodel
+class UiTask {
+public:
+    UiTask(QueueHandle_t controller_queue, QueueHandle_t ui_queue, EventGroupHandle_t events, std::shared_ptr<DeviceInfoStorage<UiDeviceRecord>> storage);
+
+    UiModel &model() { return ui_model; }
+
+private:
+    static void runner(void *params);
+    void run();
+
     QueueHandle_t ui_queue;
-    EventGroupHandle_t events;
-
+    EventGroupHandle_t event_group;
+    UiModel ui_model;
+    TaskHandle_t handle{nullptr};
 };
-
-void ui_task(void *params);
 
 #endif // UI_TASK_H
