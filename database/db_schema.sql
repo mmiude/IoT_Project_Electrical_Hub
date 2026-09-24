cbd0744067f04d4ae782da85fb782193bb368c71
Aleksi1212 <aleksinoro@gmail.com>
2026-09-24 21:43:57 +0300

    update database

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -4,23 +4,34 @@ CREATE TABLE "hub"(
 );
 
 CREATE TABLE "device"(
-    "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "id" BIGINT PRIMARY KEY,
     "hub_id" VARCHAR(255) NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255),
     "priority" VARCHAR(255) CHECK ("priority" IN ('LOW', 'MED', 'HIGH')) NOT NULL DEFAULT 'HIGH',
     "is_on" BOOLEAN NOT NULL,
     CONSTRAINT "device_hub_id_foreign" FOREIGN KEY ("hub_id") REFERENCES "hub"("id")
 );
+CREATE OR REPLACE FUNCTION set_default_device_name()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.name IS NULL THEN
+        NEW.name := 'device_' || NEW.id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_set_device_name
+BEFORE INSERT ON "device"
+FOR EACH ROW
+EXECUTE FUNCTION set_default_device_name();
 
 CREATE TABLE "device_readings"(
     "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "device_id" BIGINT NOT NULL,
-    "power_w" DOUBLE PRECISION,
-    "energy_J" DOUBLE PRECISION,
-    "voltage_V" DOUBLE PRECISION,
-    "current_A" DOUBLE PRECISION,
-    -- "timeperiod_from" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    "timeperiod" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "type" VARCHAR(255) CHECK ("type" IN ('DATA_TYPE_POWER', 'DATA_TYPE_ENERGY', 'DATA_TYPE_VOLTAGE', 'DATA_TYPE_CURRENT')) NOT NULL DEFAULT 'DATA_TYPE_POWER',
+    "value" DOUBLE PRECISION,
+    "timeperiod" TIMESTAMP(0) WITHOUT TIME ZONE,
     CONSTRAINT "device_readings_device_id_foreign" FOREIGN KEY ("device_id") REFERENCES "device"("id")
 );
 
