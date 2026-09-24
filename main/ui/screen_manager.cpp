b8d993e25c4efd082497d2d02b7d25162a197527
reneki <rene.kilpelainen@gmail.com>
2026-09-24 10:52:17 +0300

    clean up

--- main/ui/screen_manager.cpp
+++ main/ui/screen_manager.cpp
@@ -2,6 +2,7 @@
 #include "home_screen.h"
 #include "device_management_screen.h"
 #include "settings_screen.h"
+#include "new_device_notifier.h"
 #include "lvgl.h"
 
 static lv_obj_t *screens[3];
@@ -12,6 +13,9 @@ void screen_manager_init(UiModel &model)
     screens[(int)ScreenId::DEVICE_MANAGEMENT] = create_device_management_screen(model);
     screens[(int)ScreenId::SETTINGS] = create_settings_screen(model);
     screen_manager_show(ScreenId::HOME);
+
+    // after the screens (needs device management's popup to already exist for "yes")
+    init_new_device_notifier(model);
 }
 
 void screen_manager_show(ScreenId id)
